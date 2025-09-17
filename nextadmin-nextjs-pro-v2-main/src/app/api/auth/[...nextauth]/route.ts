dc767f75894d3d0d2da5e6abf741e41903ec95fc
AdminMel <74563324+AdminMel@users.noreply.github.com>
2025-09-17 16:05:20 -0600

    Update route.ts

--- nextadmin-nextjs-pro-v2-main/src/app/api/auth/[...nextauth]/route.ts
+++ nextadmin-nextjs-pro-v2-main/src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
 // src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth"; // <-- define esto en src/lib/auth.ts
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
+// ✅ En App Router se exportan handlers nombrados:
 export { handler as GET, handler as POST };
 
-// NextAuth necesita Node runtime (no edge)
+// ✅ NextAuth necesita Node runtime (no edge)
 export const runtime = "nodejs";
